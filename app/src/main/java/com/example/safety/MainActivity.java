d7d13361ee17b49c9232c4cd6fb1df7ee029cae9
Asus <bhanu.tripathi12@gmail.com>
2024-02-27 15:34:58 +0530

    tripathi fun added

--- app/src/main/java/com/example/safety/MainActivity.java
+++ app/src/main/java/com/example/safety/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public void tripathi()
+    {}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
